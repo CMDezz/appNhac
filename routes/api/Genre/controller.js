bc8f3bcb7fecf066a9a22e39c1ee6365aac800e8
Le Viet <Zhu@Zhu.local>
2020-06-30 18:14:28 +0700

    added some bug

--- routes/api/Genre/controller.js
+++ routes/api/Genre/controller.js
@@ -5,9 +5,39 @@ module.exports.getGenre=(req,res,next)=>{
     Genre.find()
         // .populate("IDSongs")
         // .populate("IDTopics")
+        .populate({path:"IDTopics",select:"_id TopicName"})
+    
         .then(songs=>res.status(200).json(songs))
         .catch(err=>res.status(500).json(err))
 }
+//get only name
+module.exports.getListNameGenre=(req,res,next)=>{
+    let arr = []
+    Genre.find()
+        // .populate("IDSongs")
+        // .populate("IDTopics")
+        .populate({path:"IDTopics",select:"_id TopicName"})
+        .then(songs=>{
+            songs.map(s=>arr.push(s.GenreName))
+            console.log(arr)
+            return arr
+        })
+        .then(arr=>res.status(200).json(arr))
+        .catch(err=>res.status(500).json(err))
+}
+//get genres of a topic 
+module.exports.getTopic=(req,res,next)=>{
+    const {id} = req.params;
+
+    Genre.find({IDTopics:id})
+    .populate({path:"IDTopics",select:"_id TopicName"})
+        .then(songs=>res.status(200).json(songs))
+        .catch(err=>res.status(500).json(err))
+}
+
+
+
+// .populate({path:"IDSong",select:'_id SongName SongImage SongSinger'})
 
 module.exports.createGenre=(req,res,next)=>{
     const {IDTopics,GenreName,GenreImage} = req.body;
@@ -25,4 +55,58 @@ module.exports.deleteGenreById=(req,res,next)=>{
         .then(()=>res.status(200).json({Message:"Deleted successfully"}))
         .catch(err=>res.status(500).json(err))
         
+}
+
+
+
+module.exports.deleteGenreById=(req,res,next)=>{
+    const {id} = req.params;
+    Genre.deleteOne({_id:id})
+        .then(()=>res.status(200).json({Message:"Deleted successfully"}))
+        .catch(err=>res.status(500).json(err))
+        
+}
+module.exports.deleteAllGenre=(req,res,next)=>{
+    // const {id} = req.params;
+    // console.log(id)
+    Genre.deleteMany()
+        .then(()=>res.status(200).json({Message:"Deleted successfully"}))
+        .catch(err=>res.status(500).json(err))   
+}
+
+//update tat ca, 
+module.exports.updateGenreById = (req,res,next)=>{
+    const {id} = req.params;
+
+ 
+    Genre.findById(id)
+        .then(Genre=>{
+            if(!Genre) Promise.reject("Genre not found")
+
+            Object.keys(req.body).forEach(key=>Genre[key]=req.body[key])
+            return Genre.save()
+        })
+        .then(Genre=>res.status(200).json(Genre))
+        .catch(err=>res.status(500).json(err))
+    
+
+}
+
+//them topic vao genre
+module.exports.addMoreTopics = (req,res,next)=>{
+    const {id} = req.params;
+    const topic = req.body.IDTopics
+
+ 
+    Genre.findById(id)
+        .then(Genre=>{
+            if(!Genre) Promise.reject("Genre not found")
+            
+            Genre.IDTopics.push(...topic)
+            return Genre.save()
+        })
+        .then(Genre=>res.status(200).json(Genre))
+        .catch(err=>res.status(500).json(err))
+    
+
 }
--- routes/api/Genre/index.js
+++ routes/api/Genre/index.js
@@ -3,7 +3,11 @@ const GenreController = require('./controller')
 const router= express.Router();
 
 router.get("/",GenreController.getGenre);
+router.get("/onlyName",GenreController.getListNameGenre);
 router.post("/",GenreController.createGenre);
 router.delete("/:id",GenreController.deleteGenreById)
-
+router.delete("/",GenreController.deleteAllGenre)
+router.put("/:id",GenreController.updateGenreById);
+router.patch("/:id",GenreController.addMoreTopics);
+router.get("/getGenresOfATopic/:id",GenreController.getTopic)
 module.exports=router;
